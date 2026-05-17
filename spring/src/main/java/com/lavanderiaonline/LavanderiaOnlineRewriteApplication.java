73a196d8b0492f9e8c5ca02bc0fa9734bc370930
everton <vieiraeverton33@hotmail.com>
2026-05-17 17:34:27 -0300

    feat: iniciando projeto em spring boot

---
+++ spring/src/main/java/com/lavanderiaonline/LavanderiaOnlineRewriteApplication.java
@@ -0,0 +1,13 @@
+package com.lavanderiaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavanderiaOnlineRewriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavanderiaOnlineRewriteApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/com/lavanderiaonline/LavanderiaOnlineRewriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavanderiaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavanderiaOnlineRewriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
