d1be25fd84532fd46c80085f107a75dd50845e57
everton <vieiraeverton33@hotmail.com>
2026-05-20 23:23:05 -0300

    chore: configurações do jwt

---
+++ spring/src/main/java/com/lavanderiaonline/infrastructure/security/JwtProperties.java
@@ -0,0 +1,10 @@
+package com.lavanderiaonline.infrastructure.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app.jwt")
+public record JwtProperties(
+  String secret,
+  long expirationMinutes
+) {
+}
