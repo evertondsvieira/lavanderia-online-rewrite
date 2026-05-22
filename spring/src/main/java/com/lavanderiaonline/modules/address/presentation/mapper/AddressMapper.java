fde0beae5c5f48c456d1349cd4147e11fe9387d5
everton <vieiraeverton33@hotmail.com>
2026-05-21 22:16:36 -0300

    feat: criando atualização de endereço no mapper

--- spring/src/main/java/com/lavanderiaonline/modules/address/presentation/mapper/AddressMapper.java
+++ spring/src/main/java/com/lavanderiaonline/modules/address/presentation/mapper/AddressMapper.java
@@ -3,6 +3,7 @@ package com.lavanderiaonline.modules.address.presentation.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 import com.lavanderiaonline.modules.address.domain.Address;
@@ -19,4 +20,7 @@ public interface AddressMapper {
   Address toEntity(AddressRequest request);
 
   AddressResponse toResponse(Address address);
+
+  @Mapping(target = "id", ignore = true)
+  void updateEntity(AddressRequest request, @MappingTarget Address address);
 }
